8953fc7a42cf40ef1d879e969a8a8ef4c3444f63
Aleksander <Fodster@rambler.ru>
2020-12-12 10:55:30 +0300

    FileReader
    Получерновая версия FileReader и необходимый минимум LogElement.

--- LogHelper/FileReader.cs
+++ LogHelper/FileReader.cs
@@ -1,16 +1,52 @@
 ﻿using LogHelper.Abstraction;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace LogHelper
 {
+    //TODO Перевести FileReader на работу с потоком
     public class FileReader : IReader
     {
+        private readonly string _pattern;
+
+#if DEBUG
+        public FileReader()
+        {
+            _pattern = "<date[ ]?=[ ]?\"([\\d]{4}-[\\d]{2}-[\\d]{2} [\\d]{2}:[\\d]{2}:[\\d]{2}.[\\d]{4})\"[ ]?Tag[ ]?=[ ]?\"([^\"]*)\"[ ]?Message[ ]?=[ ]?\"([^\"]*)\"[ ]?>";
+#else
+        public FileReader(string pattern)
+        {
+            _pattern = pattern;
+#endif
+        }
+
         public IEnumerable<LogElement> Open(string target)
         {
-            return new List<LogElement>()
+            var answer = new List<LogElement>();
+            using (StreamReader stream = new StreamReader(target))
             {
-                new LogElement(),
-            };
+                string file = stream.ReadToEnd();
+#if DEBUG
+                file = "<date=\"2020-01-01 00:00:00.0000\" Tag = \"ClassName\" Message=\"MessageBody\"><date=\"2020-01-01 00:00:00.0000\" Tag = \"ClassName\" Message=\"MessageBody\"><date=\"2020-01-01 00:00:00.0000\" Tag = \"ClassName\" Message=\"MessageBody\">";
+#endif
+
+                Regex regex = new Regex(_pattern, RegexOptions.Compiled);
+                MatchCollection matches = regex.Matches(file);
+                foreach (Match t in matches)
+                {
+                    try
+                    {
+                        answer.Add(new LogElement(DateTime.Parse(t.Groups[1].Value), t.Groups[2].Value, t.Groups[3].Value));
+                    }
+                    catch (Exception ex) {
+                        ;
+                    }
+                }
+
+            }
+            return answer;
         }
 
         LogType IReader.GetType()
--- LogHelper/LogElement.cs
+++ LogHelper/LogElement.cs
@@ -1,10 +1,24 @@
-﻿namespace LogHelper
+﻿using System;
+
+namespace LogHelper
 {
     public class LogElement
     {
-        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Message { get; set; }
 
         /// <summary>Возврашет значение будет ли отображаться объект</summary>
         public bool Visible { get; set; }
+
+        public LogElement(DateTime date, string tag, string message)
+        {
+            Date = date;
+            Tag = tag;
+            Message = message;
+            Visible = true;
+        }
     }
 }
